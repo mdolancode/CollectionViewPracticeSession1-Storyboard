01904e5f25c9426c8966905c4e2caba4671fd084
Matthew Dolan <mdolancode@gmail.com>
2024-11-24 14:25:13 -0800

    Initial Commit

---
+++ CollectionViewPracticeSession1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewPracticeSession1
+//
+//  Created by Matthew Dolan on 2024-11-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
