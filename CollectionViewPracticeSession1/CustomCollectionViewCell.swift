9f4f0edb675b4910880de0b5ea91b658c96ccec0
Matthew Dolan <mdolancode@gmail.com>
2024-11-24 17:41:52 -0800

    Create basic collection view, set label and cell size.

---
+++ CollectionViewPracticeSession1/CustomCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CustomCollectionViewCell.swift
+//  CollectionViewPracticeSession1
+//
+//  Created by Matthew Dolan on 2024-11-24.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var titleLabel: UILabel!
+}
--- CollectionViewPracticeSession1/ViewController.swift
+++ CollectionViewPracticeSession1/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet var collectionView: UICollectionView!
+    let data = Array(1...100)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCollectionViewCell", for: indexPath) as! CustomCollectionViewCell
+        cell.titleLabel.text = "Item \(data[indexPath.row])"
+        
+        return cell
+    }
 }
-
